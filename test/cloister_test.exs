3254408b9e755245dbc875aace15b3709700009c
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-12-18 12:19:51 +0100

    Start EPMD service in GH action.

--- test/cloister_test.exs
+++ test/cloister_test.exs
@@ -5,14 +5,14 @@ defmodule CloisterTest do
   test "multicasts" do
     Cloister.multicast(Cloister.Void, {:ping, self()})
 
-    Enum.each(1..5, fn _ -> assert_receive({:"$gen_cast", {:pong, _}}, 100) end)
+    Enum.each(1..5, fn _ -> assert_receive({:"$gen_cast", {:pong, _}}, 1_000) end)
     assert {:message_queue_len, 0} = :erlang.process_info(self(), :message_queue_len)
   end
 
   test "multicasts to one" do
     Cloister.multicast(Cloister.Void, {:ping_one, self()})
 
-    assert_receive({:"$gen_cast", {:pong, _}}, 100)
+    assert_receive({:"$gen_cast", {:pong, _}}, 1_000)
     assert {:message_queue_len, 0} = :erlang.process_info(self(), :message_queue_len)
   end
 end
