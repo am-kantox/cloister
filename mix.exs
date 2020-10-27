c00fa46c4eac56779546f0ca49b251b5367de8f9
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-10-27 11:07:12 +0100

    :paperclip: version bump

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Cloister.MixProject do
       {:test_cluster_task, "~> 0.5", only: [:dev, :test, :ci]},
       {:dialyxir, "~> 1.0.0", only: [:dev, :ci], runtime: false},
       {:credo, "~> 1.0", only: [:dev, :ci], runtime: false},
-      {:ex_doc, "~> 0.11", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
 
