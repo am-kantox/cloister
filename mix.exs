f7c7adaae9102e85fb09cd63e3460192541c6028
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-09-15 12:43:45 +0200

    Upgraded min Elixir version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Cloister.MixProject do
     [
       app: @app,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: compilers(Mix.env()),
       start_permanent: Mix.env() == :prod,
